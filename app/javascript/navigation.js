c6864a66fd892d094ac95f2b35f753859c23c5f1
nuclear1372 <aks74um71@gmail.com>
2021-09-24 19:33:10 +0900

    ヘルプメニューの表示するCSSアニメーションの実装

--- app/javascript/navigation.js
+++ app/javascript/navigation.js
@@ -1,11 +1,12 @@
-$(".openbtn1").click(function () {//ボタンがクリックされたら
+
+$(document).on('click',".openbtn1",function () {//ボタンがクリックされたら
 	$(this).toggleClass('active');//ボタン自身に activeクラスを付与し
     $("#g-nav").toggleClass('panelactive');//ナビゲーションにpanelactiveクラスを付与
     $(".circle-bg").toggleClass('circleactive');//丸背景にcircleactiveクラスを付与
 });
 
-$("#g-nav a").click(function () {//ナビゲーションのリンクがクリックされたら
-    $(".openbtn").removeClass('active');//ボタンの activeクラスを除去し
-    $("#g-nav").removeClass('panelactive');//ナビゲーションのpanelactiveクラスを除去
-    $(".circle-bg").removeClass('circleactive');//丸背景のcircleactiveクラスを除去
+$(document).on('click',"#g-nav a",function () {//ナビゲーションのリンクがクリックされたら
+  $(".openbtn").removeClass('active');//ボタンの activeクラスを除去し
+  $("#g-nav").removeClass('panelactive');//ナビゲーションのpanelactiveクラスを除去
+  $(".circle-bg").removeClass('circleactive');//丸背景のcircleactiveクラスを除去
 });
